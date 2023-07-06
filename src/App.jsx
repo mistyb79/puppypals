213ec7792186359806aac3202e9e1d1cb8179c3b
mistyb79 <mistybarnes79@gmail.com>
2023-07-06 17:03:40 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
-<div></div>;
+import { puppyList } from "./data.js";
+import { useState } from "react";
+
+function App() {
+  console.log(puppyList);
+
+  return (
+    <>
+      <div>
+        {puppies.map((puppy) => {
+          return <p>{puppy.name}</p>;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App
